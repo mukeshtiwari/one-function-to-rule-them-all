42cfee0e6b1a499fc9dcbffc705c3d2d411c5f46
Mukesh Tiwari <mukeshtiwari.iiitm@gmail.com>
2018-02-26 20:08:41 +1100

    started one function to rule them all

--- src/one_function_to_rule_them_all.clj
+++ src/one_function_to_rule_them_all.clj
@@ -1,13 +1,29 @@
 (ns one-function-to-rule-them-all)
 
 (defn concat-elements [a-seq]
-  :-)
+  (reduce concat '() a-seq))
+
+(concat-elements [])            ;=> ()
+(concat-elements [[1 2]])       ;=> (1 2)
+(concat-elements [[1 2] [3 4]]) ;=> (1 2 3 4)
 
 (defn str-cat [a-seq]
-  :-)
+  (if (empty? a-seq) ""
+      (reduce (fn [acc x] (str acc " " x))  a-seq)))
+
+
+(str-cat ["I" "am" "Legend"])  ;=> "I am Legend"
+(str-cat ["I" "am" "back"])    ;=> "I am back"
+(str-cat ["more" " " "space" ]) ;=> "more   space"
+(str-cat [])
 
 (defn my-interpose [x a-seq]
-  [:-])
+  (reduce (fn [acc y] (concat (list y) acc (list x))) (list x) a-seq))
+
+(my-interpose 0 [1 2 3])               ;=> (1 0 2 0 3)
+(my-interpose "," ["I" "me" "myself"]) ;=> ("I" "," "me" "," "myself")
+(my-interpose :a [1])                  ;=> (1)
+(my-interpose :a [])                   ;=> ()
 
 (defn my-count [a-seq]
   :-)
